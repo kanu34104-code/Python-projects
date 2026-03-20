4fd695c999d79a5a6f645bd528d8ba41fc87588b
Naveen Jangid <kanu34104@gmail.com>
2026-03-20 12:15:17 +0530

    Add files via upload

---
+++ peoject_13.py
@@ -0,0 +1,9 @@
+
+11. #BMI Calculator
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi 
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
+bmi = calculate_bmi(weight, height)
+print(f"Your BMI is: {bmi:.2f}")
